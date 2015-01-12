f88ba4030cd0aa719bffcc75570774940a4bd2f8
su-kaiyao <1250471161@qq.com>
2015-01-12 20:39:57 +0800

    Init

---
+++ theme.py
@@ -0,0 +1,14 @@
+name = 'catstrap'
+author = 'su-kaiyao'
+homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
+post_per_page = 5
+vars = {
+    "github": "su-kaiyao",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        }
+    ]
+}
